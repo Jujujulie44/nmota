04fb89db14a2bd41c40c006d83d2e65ee8935d19
Jujujulie44 <julieandre2008@live.fr>
2024-02-01 09:44:27 +0100

    footer/template

--- nmota-child/footer.php
+++ nmota-child/footer.php
@@ -7,19 +7,15 @@
 ?>
 
 	<footer>
-
 		<div class="div-footer">
-		<?php
-			if( has_nav_menu( 'nmota-footer' ) ) :
-
-				wp_nav_menu( [
-					'menu' => 'nmota-footer'
-				] );
-				
-			endif;
-		?>
+			<?php
+				if( has_nav_menu( 'nmota-footer' ) ) :
+					wp_nav_menu( [
+						'menu' => 'nmota-footer'
+					] );
+				endif;
+			?>
 		</div>
-
 	</footer>
 
 <?php wp_footer(); ?>
