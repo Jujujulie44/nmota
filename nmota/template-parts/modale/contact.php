b851d90399bcb082b3d1021603510356c38e1d15
Jujujulie44 <julieandre2008@live.fr>
2024-02-05 17:27:42 +0100

    tests

--- nmota/template-parts/modale/contact.php
+++ nmota/template-parts/modale/contact.php
@@ -3,25 +3,34 @@
  * Modal contact
  *
  * @package WordPress
- * @subpackage nathalie-mota theme
+ * @subpackage nmota theme
  */
 ?>
 
-<!-- Ajout d'une popup dans contact -->
-
-<?php
-// On récupère les champs ACF nécessaires
-
-?>
+<!-- Ajout de popup dans contact -->
 
 <div class="popup-overlay">
-		<div class="popup-header">
-            <img class="img-formulaire" src="<?php echo get_template_directory_uri() . '/assets/img/contact-formulaire.png'; ?> " alt="banniere-contact-formulaire">
+
+		<div class="popup-contact">
+			<div class="popup-container">
+            	<img class="img-formulaire" src="<?php echo get_template_directory_uri() . '/assets/img/contact-formulaire.png'; ?> " alt="contact-formulaire">
+				<div class="popup-title"></div>
+				<div class="popup-title"></div>
+			</div>
 		</div>
-		<p class="popup-informations"></p>
-		<?php
-            // On insère le formulaire de demandes de renseignements
-            echo do_shortcode('[contact-form-7 id="2186d60" title="Contact"]');
-		?>
+
+		<div class="popup-informations"></p>
+			<?php
+				// On insère le formulaire de demandes de renseignements
+				// get_field('reference')
+				$refPhoto = "";
+				if (get_field('reference')) {
+				$refPhoto = get_field('reference');
+				}; 
+
+				// insertion du formulaire
+				echo do_shortcode('[contact-form-7 id="2186d60" title="Contact"]');
+			?>
+		</div>
 </div>
 
