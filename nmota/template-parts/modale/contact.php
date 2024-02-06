a7280a61bdb2d7bf42f389ece3914495899429bc
Jujujulie44 <julieandre2008@live.fr>
2024-02-06 17:32:50 +0100

    modale

--- nmota/template-parts/modale/contact.php
+++ nmota/template-parts/modale/contact.php
@@ -9,16 +9,14 @@
 
 <!-- Ajout de popup dans contact -->
 
-<div class="popup-overlay">
+<div class="popup-overlay hidden">
 
-		<div class="popup-contact">
-			<div class="popup-container">
-            	<img class="img-formulaire" src="<?php echo get_template_directory_uri() . '/assets/img/contact-formulaire.png'; ?> " alt="contact-formulaire">
-				<div class="popup-title"></div>
-				<div class="popup-title"></div>
-			</div>
+	<div class="popup-contact">
+		<div class="popup-title__container">
+            <!--<img class="popup-close" src="<?php echo get_template_directory_uri() . '/assets/img/contact-formulaire.png'; ?> " alt="contact-formulaire">-->
+			<div class="popup-title"></div>
+			<div class="popup-title"></div>
 		</div>
-
 		<div class="popup-informations"></p>
 			<?php
 				// On insère le formulaire de demandes de renseignements
@@ -30,7 +28,8 @@
 
 				// insertion du formulaire
 				echo do_shortcode('[contact-form-7 id="2186d60" title="Contact"]');
-			?>
+				?>
 		</div>
+	</div>
 </div>
 
