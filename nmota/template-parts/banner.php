7d87a1fff5a9b385a2c2e4f51af0eae4006736eb
Jujujulie44 <julieandre2008@live.fr>
2024-02-19 12:16:05 +0100

    Mise en place banner

--- nmota/template-parts/banner.php
+++ nmota/template-parts/banner.php
@@ -1,21 +1,21 @@
 <div class="banner">
     <h1>Photographe event</h1>
     <?php
-        // Configuration des arguments de la requête pour récupérer une photo aléatoire
+        // Arrgument de la requête pour récupérer une photo random
         $photo_args = array(
-            'post_type'      => 'photo',
+            'post_type'      => 'photos',
             'posts_per_page' => 1,
             'orderby'        => 'rand', // Tri aléatoire
         );
 
-        // Initialisation de la requête WordPress avec les arguments définis
+        // Requête WordPress avec les arguments définis
         $photo_query = new WP_Query($photo_args);
         // Vérification si des photos sont disponibles
         if ($photo_query->have_posts()) {
             // Boucle à travers les photos
             while ($photo_query->have_posts()) {
                 $photo_query->the_post();
-                // Affichage de la vignette de la photo en taille complète
+                // Affichage de la photo en full size
                 echo get_the_post_thumbnail(get_the_ID(), 'full');
             }
             // Réinitialisation des données de publication après la boucle
