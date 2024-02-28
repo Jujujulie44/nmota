42733808956be438ee813065f0f6dcf19334d244
Jujujulie44 <julieandre2008@live.fr>
2024-02-28 13:57:15 +0100

    MAJ custom-select

--- nmota/assets/script/custom-select.js
+++ nmota/assets/script/custom-select.js
@@ -1,5 +1,5 @@
 // Attend que le document soit prêt avant d'appliquer les fonctionnalités
-$(function () {
+jQuery(function ($) {
     // Initialise le plugin Select2 sur les éléments avec la classe ".custom-select"
     $(".custom-select").select2({
       // Définit la position du menu déroulant en dessous
