431a66dc931cdf222272c70249e5a1c0c6b34a17
Jujujulie44 <julieandre2008@live.fr>
2024-03-05 19:29:25 +0100

    MAJ front

--- nmota/front-page.php
+++ nmota/front-page.php
@@ -1,9 +1,12 @@
 <?php get_header();?>
 
+<!-- Bloc pour le chargement de la banniere et du titre -->
 <?php get_template_part('template-parts/banner'); ?>
 
+<!-- Bloc pour le chargement des filtres -->
 <?php get_template_part('template-parts/filtres'); ?>
 
+<!-- Bloc pour le chargement Pour la section photos -->
 <section id="containerPhoto" class="blocCatalogue">
   <?php get_template_part('template-parts/section-photo'); ?>
 </section>
